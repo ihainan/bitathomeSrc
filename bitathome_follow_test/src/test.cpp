2b7081ebdd86c49780ee14883974c0147857c73d
ihainan <ihainan72@gmail.com>
2014-04-30 00:15:50 -0700

    修正三维坐标转为二维坐标中的Bug

--- bitathome_follow_test/src/test.cpp
+++ bitathome_follow_test/src/test.cpp
@@ -4,7 +4,11 @@
 #include <image_geometry/pinhole_camera_model.h>
 #include <tf/transform_listener.h>
 #include <opencv/cv.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include <boost/foreach.hpp>
+#include <sensor_msgs/image_encodings.h>
 #include <map>
 #include <cmath>
 #include <list>
@@ -12,22 +16,20 @@
 using namespace std;
 
 enum SkeletonState{
-	NONE,					// 没有检测到骨架
-	STATIC,					// OpenNI Tracker 认为人没丢，但无法计算骨架位置
-	TRACKING,				// 正在被跟踪
+		NONE,					// 没有检测到骨架
+		STATIC,					// OpenNI Tracker 认为人没丢，但无法计算骨架位置
+		TRACKING,				// 正在被跟踪
 };
 
 // 骨架类
 class KinectSkeleton
 {
-	public:
-		map<string, cv::Point3d> points3D;					// 三维坐标点
-		map<string, cv::Point2d> points2D;					// 二维坐标点（图像上）
-		int userID;											// 用户 ID
-		SkeletonState state;								// 骨架当前状态
+		public:
+				map<string, cv::Point3d> points3D;					// 三维坐标点
+				map<string, cv::Point2d> points2D;					// 二维坐标点（图像上）
+				int userID;											// 用户 ID
+				SkeletonState state;								// 骨架当前状态
 };
-
-
 
 // 骨架视觉操作类
 class KinectSkeletonVision
@@ -37,20 +39,35 @@ class KinectSkeletonVision
 		image_transport::CameraSubscriber sub_;		// 订阅返回对象
 		tf::TransformListener tf_listener_;			// 监听骨架 tf
 		image_geometry::PinholeCameraModel cam_model_;			// 坐标转换
-		list<KinectSkeleton> skeletons;	
+		list<KinectSkeleton> skeletons;				// 骨架
 		list<KinectSkeleton> lastSkeletons;	
 
-	public :
+		// OpenCV 相关
+
+		public :
 		// 构造函数
 		KinectSkeletonVision():it_(nh_){
 				// 订阅图片主题
-				string image_topic = nh_.resolveName("/camera/rgb/image_raw");	
+				string image_topic = nh_.resolveName("/camera/rgb/image_color");	
 				sub_ = it_.subscribeCamera(image_topic, 1, &KinectSkeletonVision::imageCb, this);
 		}
 
 
 		// 每收到一张图像
 		void imageCb(const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info_msg){
+				// 图片转换为 OpenCV 格式
+				cv_bridge::CvImagePtr cv_ptr;
+				try
+				{
+						cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+				}
+				catch (cv_bridge::Exception& e)
+				{
+						ROS_ERROR("cv_bridge exception: %s", e.what());
+						return;
+				}
+
+				// 获取骨架
 				cam_model_.fromCameraInfo(info_msg);				// 初始化转换器
 				skeletons.clear();									// 清空骨架存储
 
@@ -85,7 +102,7 @@ class KinectSkeletonVision
 						}
 						// 检测时间，如果长久不更新，代表骨架已经丢失
 						tf::Point pt = transform.getOrigin();
-						cv::Point3d pt_cv(pt.x(), pt.y(), pt.z());
+						cv::Point3d pt_cv(-pt.y(), -pt.z(), pt.x());
 						cv::Point2d uv;
 						uv = cam_model_.project3dToPixel(pt_cv);
 
@@ -122,17 +139,19 @@ class KinectSkeletonVision
 
 				// 注意，此处得到的值为 z, x, y
 				if (!skeletons.empty()){
-						cout << "检测到骨架" << endl;
+						// cout << "检测到骨架" << endl;
 						list<KinectSkeleton>::iterator v;
 						for(v = skeletons.begin(); v != skeletons.end(); ++v){
 								KinectSkeleton s = *v;
 								if(s.state == STATIC){
-										cout << "骨架" << s.userID << " 静止" << endl;
+										// cout << "骨架" << s.userID << " 静止" << endl;
 								}
 								else{
 										cout << "骨架" << s.userID << " : ";
 										cv::Point3d points = s.points3D["torso_"];
-										cout << "(" << points.x << ", " << points.y << ", " << points.z << ")" << endl;
+										// cout << "(" << points.x << ", " << points.y << ", " << points.z << ")" << endl;
+										cout << s.points2D["torso_"].x << " " << s.points2D["torso_"].y << endl;
+										cv::circle(cv_ptr -> image, s.points2D["torso_"], 3, CV_RGB(255,0,0), -1);
 								}
 						}
 				}
@@ -142,29 +161,30 @@ class KinectSkeletonVision
 
 				// 存储骨架，用于检测骨架是否静态丢失
 				lastSkeletons = skeletons;
+				cv::imshow("image", cv_ptr->image);
+				cv::waitKey(3);
 		}
 
 		// 检测骨架是否处于静态状态
 		bool checkIsStatic(KinectSkeleton skeleton, list<KinectSkeleton> lastSkeletons){
-			list<KinectSkeleton>::iterator v;
-			for(v = lastSkeletons.begin(); v != lastSkeletons.end(); ++v){
-				KinectSkeleton s = *v;
-				cv::Point3d point = s.points3D["torso_"];
-				cv::Point3d lastPoint = skeleton.points3D["torso_"];
-				if(fabs(point.x - lastPoint.x) <= 0.000001 && fabs(point.y - lastPoint.y) <= 0.0000001 && fabs(point.z - lastPoint.z) <= 0.0000001){
-						cout << point.x << " " << point.y << endl;
-						return true;
+				list<KinectSkeleton>::iterator v;
+				for(v = lastSkeletons.begin(); v != lastSkeletons.end(); ++v){
+						KinectSkeleton s = *v;
+						cv::Point3d point = s.points3D["torso_"];
+						cv::Point3d lastPoint = skeleton.points3D["torso_"];
+						if(fabs(point.x - lastPoint.x) <= 0.000001 && fabs(point.y - lastPoint.y) <= 0.0000001 && fabs(point.z - lastPoint.z) <= 0.0000001){
+								return true;
+						}
 				}
-			}
-			return false;
+				return false;
 		}
 
 		// 输出 map
 		void printMap(map<string, cv::Point3d> mymap){
-			map<string, cv::Point3d>::const_iterator itr;
-			for(itr = mymap.begin(); itr != mymap.end(); ++itr){
-				cout << "Key: " << (*itr).first << " Value: " << (*itr).second;
-			}
+				map<string, cv::Point3d>::const_iterator itr;
+				for(itr = mymap.begin(); itr != mymap.end(); ++itr){
+						cout << "Key: " << (*itr).first << " Value: " << (*itr).second;
+				}
 		}
 
 		// 判断是否是骨架 TF
